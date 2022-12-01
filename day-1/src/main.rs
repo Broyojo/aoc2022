41bfb8f0c45ec8964687aec95af9fde991dc17ff
Broyojo <dha@xoba.com>
2022-12-01 01:19:36 -0500

    improved readability of normal solutions

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -10,11 +10,18 @@ fn main() {
 fn part_one() {
     let text = fs::read_to_string("input.txt").unwrap();
     let sections: Vec<&str> = text.split("\n\n").collect();
-    let max: u64 = sections
-        .iter()
-        .map(|s| s.split('\n').map(|c| c.parse::<u64>().unwrap()).sum())
-        .max()
-        .unwrap();
+
+    let mut max = 0;
+    for section in sections {
+        let mut total = 0;
+        for line in section.split('\n') {
+            total += line.parse::<u64>().unwrap();
+        }
+        if total > max {
+            max = total;
+        }
+    }
+
     println!("{}", max);
 }
 
@@ -31,12 +38,20 @@ fn part_one_functional() {
 fn part_two() {
     let text = fs::read_to_string("input.txt").unwrap();
     let sections: Vec<&str> = text.split("\n\n").collect();
-    let mut totals: Vec<u64> = sections
-        .iter()
-        .map(|s| s.split('\n').map(|c| c.parse::<u64>().unwrap()).sum())
-        .collect();
+
+    let mut totals = vec![];
+    for section in sections {
+        let mut total = 0;
+        for line in section.split('\n') {
+            total += line.parse::<u64>().unwrap();
+        }
+        totals.push(total);
+    }
+
     totals.sort();
+
     let top: u64 = totals.iter().rev().take(3).sum();
+
     println!("{}", top);
 }
 
